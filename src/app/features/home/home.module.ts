45aa6ceb60a157c7a67fff985b88e3bff3916123
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-08-29 21:10:49 +0400

    add home module

---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { InfoComponent } from './components/info/info.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { Banner2Component } from './components/banner2/banner2.component';
+import { NotebooksComponent } from './components/notebooks/notebooks.component';
+import { SmartphonesComponent } from './components/smartphones/smartphones.component';
+import { CategoryComponent } from './components/category/category.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ProductCardModule } from '../../shared/product-card/product-card.module';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    BannerComponent,
+    InfoComponent,
+    OffersComponent,
+    Banner2Component,
+    NotebooksComponent,
+    SmartphonesComponent,
+    CategoryComponent,
+    BrandsComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule, ProductCardModule],
+})
+export class HomeModule {}
