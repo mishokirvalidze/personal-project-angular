f52f5eda367cc90bbc684f30c706d4ab3ae4d118
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-09-02 23:48:43 +0400

    add info component for home module

---
+++ src/app/features/home/components/info/info.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InfoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
