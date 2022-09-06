b2ad6f1434cb0c1879b853a3786bcb422bcee496
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-09-06 22:20:46 +0400

    add faqs directive

---
+++ src/app/features/faqs/directive/faqs.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FaqsDirective } from './faqs.directive';
+
+describe('FaqsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FaqsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/features/faqs/faqs.module.ts
+++ src/app/features/faqs/faqs.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FaqsComponent } from './faqs/faqs.component';
+import { FaqsDirective } from './directive/faqs.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FaqsComponent,
+    FaqsDirective
+  ],
   imports: [
     CommonModule
   ]
