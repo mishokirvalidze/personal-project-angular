2fd0dda4eb0290a2277cd09379155c38b597897f
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-09-06 22:21:46 +0400

    add contact component

--- src/app/features/contact/contact.module.ts
+++ src/app/features/contact/contact.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from './contact.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactComponent
+  ],
   imports: [
     CommonModule
   ]
