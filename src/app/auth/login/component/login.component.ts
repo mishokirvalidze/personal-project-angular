7a291c96c462aac8380e0729efbe50f12336d06f
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-09-07 16:37:27 +0400

    finish login component

--- src/app/auth/login/component/login.component.ts
+++ src/app/auth/login/component/login.component.ts
@@ -1,16 +1,59 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IloginForm } from '../model/login.model';
+import { SharedService } from '../../../shared/service/shared.service';
+import { Ilogin } from '../../../shared/model/shared.model';
+import { BehaviorSubject, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor(private service: SharedService, private router: Router) {}
 
   ngOnInit(): void {
+    this.service.error.pipe(tap((data) => this.errors$.next(data))).subscribe();
+    if (this.service.isLoggedIn()) {
+      this.router.navigateByUrl('/');
+    }
   }
 
+  ngOnDestroy(): void {
+    this.errors$.next('');
+  }
+
+  private errors$ = new BehaviorSubject<string>('');
+  public errors = this.errors$.asObservable();
+
+  loginForm = new FormGroup<IloginForm>({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  public submit(): void {
+    this.service.loginRegister(this.loginForm.value as Ilogin, '/login');
+  }
+
+  get email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
 }
