c5c2eddba65889fe67110cd196b92c0c68eef1c9
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-09-07 16:24:18 +0400

    finish category component

---
+++ src/app/features/home/components/category/category.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoryComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
