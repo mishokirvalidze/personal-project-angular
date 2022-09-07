d083072b4f20af3d1f9182b73b6ac98080c35137
mishokirvalidze7@gmail.com <mishokirvalidze7@gmail.com>
2022-09-07 16:38:24 +0400

    finish login module

--- src/app/auth/login/login-routing.module.ts
+++ src/app/auth/login/login-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', component: LoginComponent, title: 'Log in' },
+      { path: '', component: LoginComponent, title: 'Megabyte - Login' },
     ]),
   ],
   exports: [RouterModule],
--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './component/login.component';
+import { LoginRoutingModule } from './login-routing.module';
+import { SharedModule } from '../../shared/modules/shared.module';
+import { SharedService } from '../../shared/service/shared.service';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, LoginRoutingModule, SharedModule],
+  providers: [SharedService],
 })
 export class LoginModule {}
