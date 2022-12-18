87e5a063d4b01352f0d14cd28f9404ca6f13103c
mishokirvalidze <103035202+mishokirvalidze@users.noreply.github.com>
2022-12-18 20:51:58 +0400

    update test

--- src/app/auth/login/component/login.component.spec.ts
+++ src/app/auth/login/component/login.component.spec.ts
@@ -1,22 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
+import { SharedService } from '../../../shared/service/shared.service';
+import { Ilogin } from '../../../shared/model/shared.model';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let service: SharedService;
+  let serviceSpy: jest.SpyInstance;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [LoginComponent],
+      providers: [SharedService],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    component = TestBed.inject(LoginComponent);
+    service = TestBed.inject(SharedService);
+  }));
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a form with email and password controls', () => {
+    expect(component.loginForm.get('email')).toBeTruthy();
+    expect(component.loginForm.get('password')).toBeTruthy();
+  });
+
+  it('should call the login method of the service on form submission', fakeAsync(() => {
+    serviceSpy = jest.spyOn(service, 'loginRegister');
+    component.loginForm.patchValue({
+      email: 'test@example.com',
+      password: 'password',
+    });
+    component.submit();
+    tick();
+    expect(serviceSpy).toHaveBeenCalledWith(
+      { email: 'test@example.com', password: 'password' } as Ilogin,
+      '/login'
+    );
+  }));
 });
+
